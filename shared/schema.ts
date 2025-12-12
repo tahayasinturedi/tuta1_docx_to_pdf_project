99fb6c46782a0549e4c9898ed4175e7077680e3b
tahayasinturedi <tyasinturedi@gmail.com>
2025-12-12 13:07:28 +0100

    complete schema.ts

--- shared/schema.ts
+++ shared/schema.ts
@@ -10,5 +10,24 @@ export interface Conversion {
     pdfS3Key: string | null;
     pdfSize?: number | null;
     error: string | null; 
+    createdAt: Date;
+    completedAt:Date | null;
 
 }
+
+export type InsertConversion = Omit<Conversion, "id" | "createAt">;
+
+export type UpdateConversion = Partial<Omit<Conversion, "id" | "createdAt" >>;
+
+export const insertConversionSchema = z.object({
+    filename: z.string().min(1),
+    originalSize: z.number().positive(),
+    status: z.enum(["uploading", "converting", "completed", "failed"]),
+    progress: z.number().min(0).max(100),
+    s3Key: z.string(),
+    pdfS3Key: z.string().nullable(),
+    pdfSize: z.number().nullable().optional(),
+    error: z.string().nullable(),
+});
+
+export const updateConversionSchema = insertConversionSchema.partial();
